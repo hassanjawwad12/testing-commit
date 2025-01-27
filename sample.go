e0b3e10baae83157eb0c95d93d54a9bc983f30fe
Muhammad Hassan Jawwad <113373773+hassanjawwad12@users.noreply.github.com>
2025-01-27 15:55:56 +0500

    Update sample.go

--- sample.go
+++ sample.go
@@ -1,3 +1,9 @@
 type error interface {
     Error() string
 }
+
+type PathError struct {
+    Op string    // "open", "unlink", etc.
+    Path string  // The associated file.
+    Err error    // Returned by the system call.
+}
